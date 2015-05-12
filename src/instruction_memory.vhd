0861bd1c484a1c91b50acbfab3b5e1d0e5d983f4
VB <vladimir_belinski@hotmail.com>
2015-05-12 15:13:25 -0300

    Created instruction memory module

---
+++ src/instruction_memory.vhd
@@ -0,0 +1,25 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+entity instruction_memory is
+    port(
+       ra: in std_logic_vector(4 downto 0);
+       i: out std_logic_vector(31 downto 0)
+    );
+end instruction_memory;
+
+architecture behavior of instruction_memory is
+    
+    type rbank is array(0 to 255) of std_logic_vector(31 downto 0);
+    signal bank: rbank;
+    
+begin
+    
+    process(ra) is
+    begin
+        i <= bank(conv_integer(ra));
+    end process;
+    
+end behavior;
