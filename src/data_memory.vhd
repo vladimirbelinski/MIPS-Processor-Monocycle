5a5bfe71593c59c3161fc2804150b8685fbf11da
Gabriel Galli <g7.galli96@gmail.com>
2015-05-13 22:37:30 -0300

    Now using ieee.numeric_std

--- src/data_memory.vhd
+++ src/data_memory.vhd
@@ -1,31 +1,30 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_arith.all;
-use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
 
 entity data_memory is
     port(
 	   MemRead, MemWrite: in std_logic;
-       address: in std_logic_vector(4 downto 0);
-       wr_data: in std_logic_vector(31 downto 0);
-       r_data: out std_logic_vector(31 downto 0)
+       address: in UNSIGNED(4 downto 0);
+       wr_data: in SIGNED(31 downto 0);
+       r_data: out SIGNED(31 downto 0)
     );
 end data_memory;
 
 architecture behavior of data_memory is
-    
-    type rbank is array(0 to 255) of std_logic_vector(31 downto 0);
+
+    type rbank is array(0 to 255) of SIGNED(31 downto 0);
     signal bank: rbank;
-    
+
 begin
 
 	process(MemRead, MemWrite) is
     begin
-		if rising_edge(MemRead) then
-			r_data <= bank(conv_integer(address));
-		elsif rising_edge(MemWrite) then
-			bank(conv_integer(address)) <= wr_data;
+		if RISING_EDGE(MemRead) then
+			r_data <= bank(TO_INTEGER(address));
+		elsif RISING_EDGE(MemWrite) then
+			bank(TO_INTEGER(address)) <= wr_data;
 		end if;
     end process;
-    
+
 end behavior;
