3f0f381a97993ac5a33ba610ba2ba324b810d333
Gabriel Galli <g7.galli96@gmail.com>
2015-05-14 14:25:52 -0300

    Updated input signals

--- src/concatenation.vhd
+++ src/concatenation.vhd
@@ -4,7 +4,8 @@ use ieee.numeric_std.all;
 
 entity concatenator is
     port(
-		pc_31_28, ins_25_0: in signed(31 downto 0);
+		pc_31_28: in signed(3 downto 0);
+		ins_25_0: in signed(25 downto 0);
         j: out signed(31 downto 0)
     );
 end concatenator;
@@ -14,7 +15,7 @@ begin
 
 	process(pc_31_28, ins_25_0) is
     begin
-		j <= pc_31_28(31 downto 28) & ins_25_0(25 downto 0) & "00";
+		j <= pc_31_28 & ins_25_0 & "00";
     end process;
 
 end behavior;
