f39435a9b20e3bfb464a275aa337654d47244a08
Gabriel Galli <g7.galli96@gmail.com>
2015-05-14 15:29:41 -0300

    Updated Fibonacci to calculate up to max numbers

--- asm/fibonacci.asm
+++ asm/fibonacci.asm
@@ -1,58 +1,50 @@
-# Implemente o algoritmo de Fibonacci utilizando o conjunto de instruções do MIPS.
-# Sabendo que o registrador $s0 armazena um número "m", calcule todos os números de fibonacci menores que "m".
+# Fibonacci utilizando o conjunto de instruções do MIPS.
 
 		.data
 
-max:	.asciiz		"Digite o valor maximo do Fibonacci: "
-lib:	.byte		'\n'
-varA:	.word		0
-varB:	.word		1
-varC:	.word		1
+lib:	.asciiz		"\n"
 
 ########################################################################
 
 		.text
 
 main:							# inicializações:
-		la $t5, varA			# $t5 = &varA
-		la $t6, varB			# $t6 = &varB
-		la $t7, varC			# $t7 = &varC
-		lw $t1, 0($t5)			# $t1 = varA
-		lw $t2, 0($t6)			# $t2 = varB
-		lw $t3, 0($t7)			# $t3 = varC
-
-		li $v0, 4				# código da syscall pra leitura de string
-		la $a0, max				# coloca o endereço da string em $a0
-		syscall 				# printa 'max' no console
-
-		li $v0, 5
-		syscall
-		add $t0, $0, $v0		# armazena o valor max lido do teclado em $t0
-
+		addi $t0, $0, 10		# max
+		addi $t1, $0, 0			# a
+		addi $t2, $0, 1			# b
+		add $t3, $0, $0			# c
+		add $t5, $0 $0			# for counter
+		
 		li $v0, 1
 		add $a0, $0, $t1
-		syscall					# printa a (0)
-
+		syscall					# printa a
 		li $v0, 4
 		la $a0, lib
-		syscall					# printa '\n'
+		syscall					# printa "\n"
+		
+		li $v0, 1
+		add $a0, $0, $t2
+		syscall					# printa b
+		li $v0, 4
+		la $a0, lib
+		syscall					# printa "\n"
 
 for:							# $t0 = max, $t3 = c
-		slt $t4, $t3, $t0		# fib < m ? $t4 = 1 : $t4 = 0
+		slt $t4, $t5, $t0		# i < max ? $t4 = 1 : $t4 = 0
         beq $t4, $0, rof		# ^ if ($t4 == 0) goto rof
 
+        add $t3, $t1, $t2		# $t3 = c = a + b, ou fib(n) = fib(n - 2) + fib(n - 1)
+        add $t1, $t2, $0		# a = b
+        add $t2, $t3, $0		# b = c
+        addi $t5, $t5, 1		# i++
+        
         li $v0, 1
 		add $a0, $0, $t3
 		syscall					# printa c
-
-        add $t3, $t1, $t2		# $t3 = c = a + b, ou fib(n) = fib(n - 2) + fib(n - 1)
-
 		li $v0, 4
 		la $a0, lib
-		syscall					# printa '\n'
-
-        add $t1, $t2, $0		# a = b
-        add $t2, $t3, $0		# b = c
+		syscall					# printa "\n"
+        
         j for					# volta pro for pra ver se continua ou acabou o laço
 
 rof:    li $v0, 10				# código da syscall pra terminar a main
