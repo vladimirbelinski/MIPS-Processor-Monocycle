3cace0158f22bb7c07bd22b578b04feb88c88acd
Gabriel Galli <g7.galli96@gmail.com>
2015-05-15 18:40:46 -0300

    Writing data works

--- src/memory.vhd
+++ src/memory.vhd
@@ -18,12 +18,12 @@ architecture behavior of memory is
 
 begin
 
-	process (MemRead, MemWrite) is
-    begin
-		if rising_edge(MemRead) then
+	process (MemRead, MemWrite, address, wr_data) is
+	begin
+		if MemRead = '1' then
             r_data <= bank(to_integer(address)) & bank(to_integer(address + 1)) & bank(to_integer(address + 2)) & bank(to_integer(address + 3));
-		elsif rising_edge(MemWrite) then
-			bank(to_integer(address)) <= wr_data(31 downto 24);
+		elsif MemWrite = '1' then
+			bank(to_integer(address))     <= wr_data(31 downto 24);
 			bank(to_integer(address + 1)) <= wr_data(23 downto 16);
 			bank(to_integer(address + 2)) <= wr_data(15 downto 8);
 			bank(to_integer(address + 3)) <= wr_data(7 downto 0);
--- src/memory_tb.vhd
+++ src/memory_tb.vhd
@@ -3,20 +3,20 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use work.matrix.all;
 
-entity data_memory_tb is
-end data_memory_tb;
+entity memory_tb is
+end memory_tb;
 
-architecture behavior of data_memory_tb is
+architecture behavior of memory_tb is
 
 	constant clk_time: time := 50ns;
-	signal tb_MemRead, tb_MemWrite: std_logic;
-	signal tb_address: unsigned(4 downto 0);
+	signal tb_clk, tb_MemRead, tb_MemWrite: std_logic;
+	signal tb_address: unsigned(31 downto 0);
 	signal tb_wr_data, tb_r_data: signed(31 downto 0);
 
-	component data_memory is
+	component memory is
 		port(
-		   tb_clk, MemRead, MemWrite: in std_logic;
-		   address: in unsigned(4 downto 0);
+		   MemRead, MemWrite: in std_logic;
+		   address: in unsigned(31 downto 0);
 		   wr_data: in signed(31 downto 0);
 		   r_data: out signed(31 downto 0)
 		);
@@ -24,7 +24,7 @@ architecture behavior of data_memory_tb is
 
 begin
 
-	MEMORY: data_memory port map(tb_MemRead, tb_MemWrite, tb_address, tb_wr_data, tb_r_data);
+	MEM: memory port map(tb_MemRead, tb_MemWrite, tb_address, tb_wr_data, tb_r_data);
 	
 	CLK: process is
 	begin
@@ -40,14 +40,14 @@ begin
 		
 		tb_MemRead <= '0';
 		tb_MemWrite <= '0';
-		tb_address <= "00000";
-		tb_wr_data <= x"00000000"
+		tb_address <= x"00000000";
+		tb_wr_data <= x"00000000";
 		
 		tb_MemWrite <= '1';
-		for i in 0 to 255 loop -- if it's 256 bytes, the address should be 8 bits...!?
-		wait for clk_time;
-		tb_address <= tb_address + 4;
-		tb_wr_data <= tb_wr_data + 1;
+		for i in 0 to 63 loop
+			wait for clk_time;
+			tb_address <= tb_address + 4;
+			tb_wr_data <= tb_wr_data + 1;
 		end loop;
 		tb_MemWrite <= '0';
 		
