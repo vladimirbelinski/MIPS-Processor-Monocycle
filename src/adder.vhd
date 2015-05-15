c0972ac6fdc924a71795cae9f821101b8e4110b8
Gabriel Galli <g7.galli96@gmail.com>
2015-05-14 21:23:12 -0300

    Updated adder to comply with our patterns relating numeric_std

--- src/adder.vhd
+++ src/adder.vhd
@@ -7,27 +7,12 @@ use ieee.numeric_std.all;
 
 entity adder is
 	port(
-	    Cin: in std_logic;
 	    A, B: in signed(31 downto 0);
-	    O: out signed(31 downto 0);
-	    Cout: out std_logic
+	    A_plus_B: out signed(31 downto 0)
 	);
 end entity adder;
 
 architecture add of adder is
 begin
-	process (A, B, Cin)
-		variable ad: signed (31 downto 0);
-		variable C: std_logic;
-	begin
-
-	C := Cin;
-
-	for i in 0 to 31 loop
-		ad(i) := (A(i) xor B(i) xor C);
-		C :=(A(i) and B(i))or((A(i) xor B(i)) and C);
-	end loop;
-	Cout <= C;
-	O <= ad;
-	end process;
+	A_plus_B <= A + B;
 end add;
