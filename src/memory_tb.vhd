f13a29a2dc06846cedd0a9a845b5608309b1ba40
Gabriel Galli <g7.galli96@gmail.com>
2015-05-15 20:24:22 -0300

    Fixed spacing between number and unit

--- src/memory_tb.vhd
+++ src/memory_tb.vhd
@@ -8,7 +8,7 @@ end memory_tb;
 
 architecture behavior of memory_tb is
 
-	constant clk_time: time := 50ns;
+	constant clk_time: time := 50 ns;
 	signal tb_clk, tb_MemRead, tb_MemWrite: std_logic;
 	signal tb_address: unsigned(31 downto 0);
 	signal tb_wr_data, tb_r_data: signed(31 downto 0);
@@ -25,7 +25,7 @@ architecture behavior of memory_tb is
 begin
 
 	MEM: memory port map(tb_MemRead, tb_MemWrite, tb_address, tb_wr_data, tb_r_data);
-	
+
 	CLK: process is
 	begin
 		tb_clk <= '0';
@@ -33,16 +33,16 @@ begin
 		tb_clk <= '1';
 		wait for (clk_time / 2);
 	end process CLK;
-	
+
 	TB: process is
 		variable i: natural;
 	begin
-		
+
 		tb_MemRead <= '0';
 		tb_MemWrite <= '0';
 		tb_address <= x"00000000";
 		tb_wr_data <= x"00000000";
-		
+
 		tb_MemWrite <= '1';
 		for i in 0 to 63 loop
 			wait for clk_time;
@@ -52,16 +52,16 @@ begin
 		tb_MemWrite <= '0';
 
 		----------------------------------------------------------------
-		
+
 		tb_address <= x"00000000";
-		
+
 		tb_MemRead <= '1';
 		for i in 0 to 63 loop
 			wait for clk_time;
 			tb_address <= tb_address + 4;
 		end loop;
 		tb_MemRead <= '0';
-		
+
 	end process TB;
-	
+
 end behavior;
--- src/register_bank_tb.vhd
+++ src/register_bank_tb.vhd
@@ -8,7 +8,7 @@ end register_bank_tb;
 
 architecture behavior of register_bank_tb is
 
-	constant clk_time: time := 10ns;
+	constant clk_time: time := 10 ns;
 	signal tb_clk, tb_RegWrite: std_logic;
 	signal tb_read1, tb_read2, tb_write_address: unsigned(4 downto 0);
 	signal tb_write_data, tb_data1, tb_data2: signed(31 downto 0);
@@ -25,7 +25,7 @@ architecture behavior of register_bank_tb is
 begin
 
 	RB: register_bank port map(tb_RegWrite, tb_read1, tb_read2, tb_write_address, tb_write_data, tb_data1, tb_data2);
-	
+
 	CLK: process is
 	begin
 		tb_clk <= '0';
@@ -33,17 +33,17 @@ begin
 		tb_clk <= '1';
 		wait for (clk_time / 2);
 	end process CLK;
-	
+
 	TB: process is
 		variable i: natural;
 	begin
-		
+
 		tb_RegWrite <= '0';
 		tb_read1 <= "00000";
 		tb_read2 <= "00000";
 		tb_write_address <= "00000";
 		tb_write_data <= x"00000000";
-		
+
 		tb_RegWrite <= '1';
 		for i in 0 to 31 loop
 			wait for clk_time;
@@ -51,18 +51,17 @@ begin
 			tb_write_data <= tb_write_data + 1;
 		end loop;
 		tb_RegWrite <= '0';
-		
+
 		----------------------------------------------------------------
-		
+
 		tb_read1 <= "00000";
 		tb_read2 <= "10000";
-		
+
 		for i in 0 to 15 loop
 			wait for clk_time;
 			tb_read1 <= tb_read1 + 1;
 			tb_read2 <= tb_read2 + 1;
 		end loop;
 	end process TB;
-	
-end behavior;
 
+end behavior;
