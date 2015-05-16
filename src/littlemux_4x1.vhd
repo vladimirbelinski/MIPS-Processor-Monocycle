ca9711456e614128000424ed9fc494f0534d8f3b
vladimirbelinski <vladimir_belinski@hotmail.com>
2015-05-16 17:13:40 -0300

    Created 4-bit mux and updated concatenation

--- src/littlemux_4x1.vhd
+++ src/littlemux_4x1.vhd
@@ -5,13 +5,13 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity mux_4x1 is
+entity littlemux_4x1 is
     port(
         sel1, sel2: in std_logic;
-        I00, I01, I10, I11: in signed(31 downto 0);
-        O: out signed(31 downto 0)
+        I00, I01, I10, I11: in signed(4 downto 0);
+        O: out signed(4 downto 0)
     );
-end mux_4x1;
+end littlemux_4x1;
 
 architecture behavior of mux_4x1 is
 begin
