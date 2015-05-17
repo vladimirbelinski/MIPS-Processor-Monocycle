9d680d7f9597b305252a128b906d8435635c9790
vladimirbelinski <vladimir_belinski@hotmail.com>
2015-05-16 22:58:02 -0300

    Updated process

--- src/ULA.vhd
+++ src/ULA.vhd
@@ -16,29 +16,26 @@ entity ULA is
 end ULA;
 
 architecture behavior of ULA is
-
-	signal result: signed(31 downto 0);
-
 begin
     process (ALU_control, A, B) is
+      variable result: signed(31 downto 0);
     begin
         case ALU_control is
-            when "000"  => result <= A and B;
-            when "001"  => result <= A or B;
-            when "110"  => result <= A - B;
+            when "000"  => result := A and B;
+            when "001"  => result := A or B;
+            when "110"  => result := A - B;
             when "111"  =>
-				if (A < B) then result <= x"00000000";
-                else result <= x"00000001";
+				        if (A < B) then result := x"00000000";
+                else result := x"00000001";
                 end if;
-             when "011"  => result <= x"00000000";
-            when others => result <= A + B;
+            when "011"  => result := x"00000000";
+            when others => result := A + B;
         end case;
 
         if (result = x"00000000") then Zero <= '1';
         else Zero <= '0';
         end if;
+      ALU_result <= result;
     end process;
-
-    ALU_result <= result;
 
 end behavior;
